67506b5ff43e7138c6a5292b1224a3744c8366d5
Oskar Przybylski <oskar.przybylski23@gmail.com>
2023-11-28 22:10:19 +0000

    generic format changes

--- src/commands.js
+++ src/commands.js
@@ -1,33 +1,25 @@
 const commandsList = {
-    ping: message => replyToMessage(message, 'Pong!'),
-    greet: message => replyToMessage(message, 'hello')
-    //joke: 
-    //echo: 
-    //help:
-}
+  ping: (message) => replyToMessage(message, "Pong!"),
+  greet: (message) => replyToMessage(message, "hello"),
+  //joke:
+  //echo:
+  //help:
+};
 
-function replyToMessage (msg, answer){
-    msg.reply(answer)
+function replyToMessage(msg, answer) {
+  msg.reply(answer);
 }
 
 //function to tell a joke
-function tellAJoke (){
-
-}
+function tellAJoke() {}
 
 // help function should list all of the commands available (object.keys(commandsList))?
-function help () {
-
-}
+function help() {}
 
 // Echo Command- when a user types !echo [message], the bot will repeat the message back to them.
-function echo () {
-
-}
+function echo() {}
 
 // Command Logging: As a developer, I want the bot to log the use of commands
-function logCommands () {
+function logCommands() {}
 
-}
-
-module.exports = { commandsList }
+module.exports = { commandsList };

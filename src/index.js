d1d25ec3fe94857024b80533b7ac9c19bd0a8e9c
Oskar Przybylski <oskar.przybylski23@gmail.com>
2023-11-29 20:33:12 +0000

    add mention command execution

--- src/index.js
+++ src/index.js
@@ -20,12 +20,25 @@ client.once("ready", (c) => {
 
 client.on("messageCreate", (message) => {
   //console.log(message);
-  if (message.author.bot || !message.content.startsWith(prefix)) {
+
+  // check if the bot has been mentioned and set botMentioned variable to true or false
+  const botMentioned = message.mentions.has(client.user.id);
+  console.log(`Bot mentioned: ${botMentioned}`);
+
+  if (
+    message.author.bot ||
+    (!message.content.startsWith(prefix) && !botMentioned)
+  ) {
+    console.log("message ignored!");
     return;
   }
- 
-  const command = message.content.slice(1);
-  //console.log(command);
+
+  // Remove the prefix or mention from the message content
+  const command = botMentioned
+    ? message.content.slice(client.user.id.length + 4).trim()
+    : message.content.slice(prefix.length).trim();
+
+  console.log(`command passed: ${command}`);
 
   if (command in commandsList) {
     commandsList[command](message);

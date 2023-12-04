534eb4be5387e903e2853fcea97586b63bab51cc
Frannerz <francesstephens@hotmail.com>
2023-12-04 11:51:09 +0000

    bot recognises dms

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const { Client, IntentsBitField, GatewayIntentBits } = require("discord.js");
+const { Client, IntentsBitField, GatewayIntentBits, Partials } = require("discord.js");
 const { commandsList, commandLog } = require("./commands");
 
 const prefix = "!";
@@ -11,25 +11,22 @@ const client = new Client({
     IntentsBitField.Flags.GuildMembers,
     IntentsBitField.Flags.MessageContent,
     GatewayIntentBits.Guilds,
+    GatewayIntentBits.DirectMessages,
   ],
+  partials: [Partials.Message, Partials.Channel, Partials.Reaction]
 });
 
-//const channel = client.channels.cache.get(process.env.CHANNEL);
+
 
 client.once("ready", (c) => {
   console.log(`${c.user.tag} is online!`);
-//   if (channel) {
-//     channel.send(`${c.user.tag} is online! Type !help to view possible commands.`);
-//   } else {
-//     console.error('Text channel not found.')
-//   }
 });
 
 client.on("messageCreate", (message) => {
   //console.log(message);
 
   // check if the bot has been mentioned and set botMentioned variable to true or false
-  const botMentioned = message.mentions.has(client.user.id);
+  const botMentioned = message.mentions.has(client.user.id)|| message.channel.type === 1;
   console.log(`Bot mentioned: ${botMentioned}`);
 
   if (
@@ -39,6 +36,20 @@ client.on("messageCreate", (message) => {
     console.log("message ignored!");
     return;
   }
+
+  //check context of message
+  if(botMentioned) {
+    if(message.content.includes('?')) {
+    message.reply("That's a good question! Let me think about it...");
+    } 
+    const greeting = ["hi", "hello", "hey"];
+    for(const greet of greeting){
+      if(message.content.includes(greet)){
+        message.reply(`Hi, ${message.author}, how can I help you?`);
+      }
+    }
+  };
+
 
   // Remove the prefix or mention from the message content
   const command = botMentioned
@@ -51,7 +62,9 @@ client.on("messageCreate", (message) => {
     commandLog.push(message.content);
     commandsList[command](message);
   }
+
 });
+
 
 
 client.login(process.env.TOKEN);
